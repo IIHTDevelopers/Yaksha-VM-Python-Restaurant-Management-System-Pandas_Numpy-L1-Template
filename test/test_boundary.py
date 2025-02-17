6ecd262c3486c9a8e76ee861414ef2a6bc81cfe7
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2025-02-17 10:56:47 +0530

    Delete test directory

--- test/test_boundary.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from test.TestUtils import TestUtils
-class BoundaryTest(unittest.TestCase):
-    def test_boundary(self):
-        test_obj = TestUtils()
-        test_obj.yakshaAssert("TestBoundary",True,"boundary")
-        print("TestBoundary = Passed")
--- test/test_exceptional.py
+++
@@ -1,7 +0,0 @@
-import unittest
-from test.TestUtils import TestUtils
-class ExceptionalTest(unittest.TestCase):
-    def test_exceptional(self):
-        test_obj = TestUtils()
-        test_obj.yakshaAssert("TestExceptional",True,"boundary")
-        print("TestExceptional = Passed")
